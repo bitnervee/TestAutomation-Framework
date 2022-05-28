61495bfdfcb305e1db8248a7047a27c22c805aae
mr-possible <sambhavd4@gmail.com>
2022-05-28 15:19:54 +0530

    Added google-code-style.xml and removed unnecessary data.

--- src/main/java/com/shashank/framework/LogMe.java
+++ src/main/java/com/shashank/framework/LogMe.java
@@ -5,53 +5,53 @@ import org.apache.logging.log4j.Logger;
 
 public class LogMe {
 
-    private final Logger log;
+  private final Logger log;
 
-    public LogMe(String className) {
-        this.log = LogManager.getLogger(className);
-    }
+  public LogMe(String className) {
+    this.log = LogManager.getLogger(className);
+  }
 
-    /**
-     * @param message This level will log the progress of the application.
-     */
-    public void info(String message) {
-        this.log.info(message);
-    }
-
-
-    /**
-     * @param message This level will show information regarding warnings, that may not stop the
-     *                execution but may still cause problems.
-     */
-    public void warn(String message) {
-        this.log.warn(message);
-    }
+  /**
+   * @param message This level will log the progress of the application.
+   */
+  public void info(String message) {
+    this.log.info(message);
+  }
 
 
-    /**
-     * @param message This level will show messages that inform users about error events that may not
-     *                stop the application.
-     */
-    public void error(String message) {
-        this.log.error(message);
-    }
+  /**
+   * @param message This level will show information regarding warnings, that may not stop the
+   *                execution but may still cause problems.
+   */
+  public void warn(String message) {
+    this.log.warn(message);
+  }
 
 
-    /**
-     * @param message This will print information critical to the system that may even crash the
-     *                application
-     */
-    public void fatal(String message) {
-        this.log.fatal(message);
-    }
+  /**
+   * @param message This level will show messages that inform users about error events that may not
+   *                stop the application.
+   */
+  public void error(String message) {
+    this.log.error(message);
+  }
 
 
-    /**
-     * @param message This level will log debugging information
-     */
-    public void debug(String message) {
-        this.log.debug(message);
-    }
+  /**
+   * @param message This will print information critical to the system that may even crash the
+   *                application
+   */
+  public void fatal(String message) {
+    this.log.fatal(message);
+  }
+
+
+  /**
+   * @param message This level will log debugging information
+   */
+  public void debug(String message) {
+    this.log.debug(message);
+  }
 
 
 }
--- src/test/java/com/shashank/FaceBookLoginTest.java
+++ src/test/java/com/shashank/FaceBookLoginTest.java
@@ -7,22 +7,22 @@ import org.testng.annotations.Test;
 
 public class FaceBookLoginTest {
 
-    /*@Test
-    @NeedWeb(browserType = "safari")
-    public void testRunOnChrome() throws InterruptedException {
-        WebDriver driver = PageDriver.getDriver();
-        System.out.println(driver);
-        driver.get("https://www.facebook.com");
-        Thread.sleep(6000);
-    }*/
+  @Test
+  @NeedWeb(browserType = "safari")
+  public void testRunOnChrome() throws InterruptedException {
+    WebDriver driver = PageDriver.getDriver();
+    System.out.println(driver);
+    driver.get("https://www.facebook.com");
+    Thread.sleep(6000);
+  }
 
-    @Test
-    @NeedWeb(browserType = "edge")
-    public void testRunOnEdge() throws InterruptedException {
-        WebDriver driver = PageDriver.getDriver();
-        System.out.println(driver);
-        driver.get("https://www.facebook.com");
-        Thread.sleep(6000);
-    }
+  @Test
+  @NeedWeb(browserType = "edge")
+  public void testRunOnEdge() throws InterruptedException {
+    WebDriver driver = PageDriver.getDriver();
+    System.out.println(driver);
+    driver.get("https://www.facebook.com");
+    Thread.sleep(6000);
+  }
 
 }
