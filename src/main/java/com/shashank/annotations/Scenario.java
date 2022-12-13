9ac04ce740dfc5cf93238adfbb095c84d24c791a
mr-possible <sambhavd4@gmail.com>
2022-12-13 12:16:29 +0530

    Added TestInfo interface, tweaked extent report info and added Scenario annotation.

---
+++ src/main/java/com/shashank/annotations/Scenario.java
@@ -0,0 +1,19 @@
+package com.shashank.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Scenario {
+
+    String tester();
+
+    String module();
+
+    String id();
+
+    String description();
+}
